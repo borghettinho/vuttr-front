742f3ec900e01798caf8b6555b657df782f3e538
Valker Vieira <valker.vieira@gmail.com>
2019-06-30 01:39:26 -0300

    Added filter state control

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { getTools } from "../api";
+import { getTools, searchTools, searchTags } from "../api";
 
 const sectionStyles = {
   display: "flex",
@@ -10,6 +10,7 @@ const sectionStyles = {
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [results, setResults] = useState([]);
+  const [filterByTag, setFilterByTag] = useState(false);
 
   useEffect(() => {
     getTools().then(results => {
@@ -17,6 +18,9 @@ const App = () => {
       setIsLoading(false);
     });
   }, []);
+
+  const handleFilter = () => setFilterByTag(!filterByTag);
+  const performSearch = value => console.log(value);
 
   return (
     <div>
@@ -28,8 +32,18 @@ const App = () => {
       <section style={sectionStyles}>
         {/* This form handles the search logic */}
         <form>
-          <input type="text" placeholder="search" />
-          <input type="checkbox" name="toggle-tag" id="toggle-tag" />
+          <input
+            type="text"
+            placeholder="search"
+            onChange={e => performSearch(e.target.value)}
+          />
+          <input
+            type="checkbox"
+            name="toggle-tag"
+            id="toggle-tag"
+            checked={filterByTag}
+            onChange={() => handleFilter()}
+          />
           <label htmlFor="toggle-tag">Search in tags only</label>
         </form>
 
@@ -40,7 +54,7 @@ const App = () => {
       <main>
         {isLoading ? <p>Loading...</p> : null}
         {results.map(result => (
-          <div key={result.key}>
+          <div key={result.id}>
             <p>Title: {result.title}</p>
           </div>
         ))}
