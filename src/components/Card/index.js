b83b85e58dc8058bd2ae3f580a5b87ea04e0c3f6
Valker Vieira <valker.vieira@gmail.com>
2019-06-30 21:07:15 -0300

    Added styled components

---
+++ src/components/Card/index.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Card = styled.div`
+  border: 1px solid #ebeaed;
+  border-radius: 5px;
+  padding: 0.5em;
+
+  &:hover {
+    box-shadow: 0 5px 7px rgba(0, 0, 0, 0.05);
+  }
+`;
+
+export { Card };
--- src/components/Tool.jsx
+++ src/components/Tool.jsx
@@ -1,36 +1,32 @@
 import React from "react";
 
-const toolStyles = {
-  border: "1px solid #eee",
-  borderRadius: "2%",
-  margin: "1.5rem 0",
-  padding: "0.5rem",
-  boxShadow: "1px 1px 3px rgba(0,0,0,0.2)"
+import { Card } from "../components/Card";
+
+const headerStyles = {
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "space-between"
 };
+
+const buttonStyles = { border: "none", outline: "none" };
 
 const Tool = props => {
   let tags = props.result.tags;
 
   return (
-    <div key={props.result.id} style={toolStyles}>
-      <header
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-between"
-        }}
-      >
+    <Card key={props.result.id}>
+      <header style={headerStyles}>
         <a href={props.result.link} target="_blank">
           {props.result.title}
         </a>
-        <button style={{ border: "none", outline: "none" }}>X remove</button>
+        <button style={buttonStyles}>X remove</button>
       </header>
       <p>{props.result.description}</p>
       <footer />
       {tags.map(tag => (
         <span>#{tag}</span>
       ))}
-    </div>
+    </Card>
   );
 };
 
